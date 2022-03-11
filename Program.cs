86fb76a6987e1e4693e4358dff28ac5f8d9a8043
NICKN1 <iranildo.junior@outlook.com>
2022-03-11 19:02:32 -0300

    Desafio 6

--- Program.cs
+++ Program.cs
@@ -57,6 +57,30 @@ namespace BeeCrowd___C_
         MEDIA = MEDIA / 11;
         Console.WriteLine("MEDIA = " + MEDIA.ToString("F5", nfi));
         }
+        static void Desafio6()
+        {
+            /*
+            Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir,
+            calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota 
+            C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+            Entrada
+            O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
+            Saída
+            Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após
+            o ponto decimal e com um espaço em branco antes e depois da igualdade.
+            */
+        NumberFormatInfo nfi = (NumberFormatInfo)
+        CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = ".";
+        
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+        double C = double.Parse(Console.ReadLine());
+        double MEDIA = 0;
+        MEDIA = (A * 2)  + (B * 3) + (C * 5);
+        MEDIA = MEDIA / 10;
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F1", nfi));
+        }
         static void Main(string[] args)
         {
             
