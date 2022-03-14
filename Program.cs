4ae333881c899a6929133082d455b5abc909d119
NICKN1 <iranildo.junior@outlook.com>
2022-03-14 15:40:05 -0300

    Desafio 9 - Salário com Bônus

--- Program.cs
+++ Program.cs
@@ -127,6 +127,33 @@ namespace BeeCrowd___C_
         Console.WriteLine("NUMBER = "+ numFuncionario);
         Console.WriteLine("SALARY = U$ "+ totalSalario.ToString("F2", nfi));
         }
+        
+        static void Desafio9 ()
+        {
+        /*
+        Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por 
+        ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, 
+        informar o total a receber no final do mês, com duas casas decimais.
+        Entrada
+        O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double) 
+        com duas casas decimais, representando o salário fixo do vendedor e montante total das vendas efetuadas 
+        por este vendedor, respectivamente.
+        Saída
+        Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+        */
+        NumberFormatInfo nfi = (NumberFormatInfo)
+        CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = ".";
+        
+        string nomeVendedor = Console.ReadLine();
+        double salarioVendedor = double.Parse(Console.ReadLine());
+        double totalVendas = double.Parse(Console.ReadLine());
+        
+        totalVendas = (totalVendas * 15) / 100;
+        salarioVendedor = totalVendas + salarioVendedor;
+        
+        Console.WriteLine("TOTAL = R$ "+ salarioVendedor.ToString("F2", nfi));
+        }
         static void Main(string[] args)
         {
         
