fb76ffc6f359985fb47277bf21ee29ce16573408
NICKN1 <iranildo.junior@outlook.com>
2022-03-14 16:15:43 -0300

    Desafio 11 - Esfera

--- Program.cs
+++ Program.cs
@@ -154,6 +154,32 @@ namespace BeeCrowd___C_
         
         Console.WriteLine("TOTAL = R$ "+ salarioVendedor.ToString("F2", nfi));
         }
+        
+        static void Desafio11()
+        {
+            /*
+            Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio 
+            (R). A fórmula para calcular o volume é: (4/3) * pi * R3. Considere (atribua) para pi o valor 3.14159.
+            Entrada
+            O arquivo de entrada contém um valor de ponto flutuante (dupla precisão), correspondente ao raio 
+            da esfera.
+            Saída
+            A saída deverá ser uma mensagem "VOLUME" conforme o exemplo fornecido abaixo, com um espaço antes 
+            e um espaço depois da igualdade. O valor deverá ser apresentado com 3 casas após o ponto.
+            */
+
+        NumberFormatInfo nfi = (NumberFormatInfo)
+        CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = ".";
+        
+        double raio = double.Parse(Console.ReadLine());
+        double volume = 0;
+        
+        raio = Math.Pow(raio, 3);
+        volume = (4/3.0) * (3.14159 * raio);
+
+        Console.WriteLine("VOLUME = "+ volume.ToString("F3", nfi));
+        }
         static void Main(string[] args)
         {
         
