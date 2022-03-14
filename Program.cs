a5cc1afc0729da6f7cf9c5eca961be0386321923
NICKN1 <iranildo.junior@outlook.com>
2022-03-14 15:28:55 -0300

    Desafio 8 - Salário

--- Program.cs
+++ Program.cs
@@ -98,9 +98,38 @@ namespace BeeCrowd___C_
         diferenca = (A * B) - (C * D);
         Console.WriteLine("DIFERENCA = "+ diferenca);
         }
+        
+        static void Desafio8()
+        {
+        /*
+        Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que 
+        recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do 
+        funcionário, com duas casas decimais.
+        Entrada
+        O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o 
+        número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, 
+        respectivamente.
+        Saída
+        Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes 
+        e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+        */
+        NumberFormatInfo nfi = (NumberFormatInfo)
+        CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = ".";
+
+        int numFuncionario = int.Parse(Console.ReadLine());
+        int numHoraTrabalhada = int.Parse(Console.ReadLine());
+        double valorHoraTrabalhada = double.Parse(Console.ReadLine());
+        double totalSalario = 0;
+        
+        totalSalario = numHoraTrabalhada * valorHoraTrabalhada;
+        
+        Console.WriteLine("NUMBER = "+ numFuncionario);
+        Console.WriteLine("SALARY = U$ "+ totalSalario.ToString("F2", nfi));
+        }
         static void Main(string[] args)
         {
-            
+        
         }
     }
 }
